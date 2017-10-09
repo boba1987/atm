d1cce95168e26957d58b249c3745ae086de168da
Slobodan Djordjevic <slobodan.djordjevic@tamedia.ch>
2017-10-09 10:39:19 +0200

    Add keyboard

--- src/components/AtmForm.js
+++ src/components/AtmForm.js
@@ -3,6 +3,17 @@ import PropTypes from 'prop-types';
 import RaisedButton from 'material-ui/RaisedButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import initialState from '../reducers/initialState';
+import Keyboard from 'react-material-ui-keyboard';
+import TextField from 'material-ui/TextField';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+const numericKeyboard = [
+    ['Escape', '', 'Backspace'],
+    ['7',      '8',         '9'],
+    ['4',      '5',         '6'],
+    ['1',      '2',         '3'],
+    ['0',      '', 'Enter']
+];
 
 class AtmForm extends React.Component {
   constructor(props, context) {
