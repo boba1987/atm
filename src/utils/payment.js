ad1d3d831000c0dde29568e73e14c7b95f4aeb28
Slobodan Djordjevic <slobodan.djordjevic@tamedia.ch>
2017-10-09 07:16:26 +0200

    Dependencies update

---
+++ src/utils/payment.js
@@ -0,0 +1,16 @@
+export function execPayment(notes, amount) {
+    let distribution = {};
+    let note;
+
+    for (let i=0; i < notes.length; i++) {
+      distribution[notes[i]] = 0;
+      while (notes[i] <= amount) {
+        amount = amount - notes[i];
+        note = distribution[notes[i]];
+        note += 1;
+        distribution[notes[i]] = note;
+      }
+    }
+
+    return distribution;
+}
